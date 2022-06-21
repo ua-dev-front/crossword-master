eb3492c273ff1844cc68fbe21736d742fbf1af84
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-06-21 23:31:30 +0300

    Add types

--- backend/generate.py
+++ backend/generate.py
@@ -1,22 +1,63 @@
 import requests
+from typing import TypedDict
 
 from app import API_PATH
 
+Answer = str | None
+Coord = list[int]
+Coords = list[Coord]
+Id = int
+Length = int
+Pattern = str
+Question = str | None
+WordType = str
 
-def set_words_id(words):
+
+class Relation(TypedDict):
+    id: Id
+    coord: Coord
+
+
+class StartWord(TypedDict):
+    answer: Answer
+    api_attempts: int
+    coords: Coords
+    length: int
+    question: Question
+    relations: list[Relation]
+    type: WordType
+
+
+class Word(StartWord):
+    id: Id
+
+
+Words = list[Word]
+
+
+class NormalizedWord(TypedDict):
+    answer: Answer
+    id: Id
+    question: Question
+    startPosition: Coord
+
+
+NormalizedWords = list[NormalizedWord]
+
+
+def set_words_id(words: Words) -> None:
     word_id = 1
     for word in words:
         word['id'] = word_id
         word_id += 1
-    return words
 
 
-def modify_existing_word(word, coord):
+def modify_existing_word(word: StartWord, coord: Coord) -> None:
     word['length'] += 1
     word['coords'].append(coord)
 
 
-def define_words_by_type(word_type, words, coord, is_x=True):
+def define_words_by_type(word_type: WordType, words: list[StartWord], coord: Coord, is_x: bool = True) -> None:
     is_new_word = True
 
     axis_coord = coord[0] if is_x else coord[1]
@@ -26,14 +67,23 @@ def define_words_by_type(word_type, words, coord, is_x=True):
             current_coord = [current_axis_coord, coord[1]] if is_x else [coord[0], current_axis_coord]
             if current_coord in word['coords'] and coord not in word['coords'] and word['type'] == word_type:
                 is_new_word = False
-                modify_existing_word(word, coord, current_coord, word_type)
+                modify_existing_word(word, coord)
 
     if is_new_word:
-        word = {'length': 1, 'coords': [coord], 'type': word_type, 'answer': None, 'api_attempts': 0}
+        word: StartWord = {
+            'length': 1,
+            'coords': [coord],
+            'type': word_type,
+            'answer': None,
+            'question': None,
+            'api_attempts': 0,
+            'relations': []
+        }
+
         words.append(word)
 
 
-def filter_words(words):
+def filter_words(words: Words) -> Words:
     return [word for word in words if word['length'] > 1]
 
 
@@ -42,15 +92,14 @@ def get_relation(word, other_word, coord):
         word['relations'].append({'id': other_word['id'], 'coord': coord})
 
 
-def get_relations(words):
+def get_relations(words: Words) -> None:
     for word in words:
-        word['relations'] = []
         for coord in word['coords']:
             for other_word in words:
                 get_relation(word, other_word, coord)
 
 
-def define_words(table):
+def define_words(table) -> Words:
     words = []
 
     for i in range(len(table)):
@@ -66,17 +115,17 @@ def define_words(table):
     return words
 
 
-def find_word_by_id(word_id, words):
+def find_word_by_id(word_id: Id, words: Words) -> Word:
     for word in words:
         if word['id'] == word_id:
             return word
 
 
-def find_coord_index(coord, coords):
+def find_coord_index(coord: Coord, coords: Coords) -> int:
     return coords.index(coord)
 
 
-def get_word_pattern(word, words):
+def get_word_pattern(word: Word, words: Words) -> Pattern:
     pattern = '?' * word['length']
     if word['relations']:
         for relation in word['relations']:
@@ -92,11 +141,11 @@ def get_word_pattern(word, words):
     return pattern
 
 
-def normalize_question(question):
+def normalize_question(question: Question) -> Question:
     return question[2:].capitalize()
 
 
-def get_answers_and_questions(words):
+def get_answers_and_questions(words: Words) -> None:
     for word in words:
         pattern = get_word_pattern(word, words)
         url = API_PATH + '?sp=' + pattern + '&md=d&max=1000'
@@ -112,25 +161,25 @@ def get_answers_and_questions(words):
             print('Error:', response.status_code)
 
 
-def divide_words_by_type(word_type, words):
+def divide_words_by_type(word_type: WordType, words: Words) -> Words:
     return [word for word in words if word['type'] == word_type]
 
 
-def is_words_valid(words):
+def is_words_valid(words) -> bool:
     for word in words:
         if word['answer'] is None or word['question'] is None:
             return False
     return True
 
 
-def is_no_solution(words):
+def is_no_solution(words) -> bool:
     for word in words:
         if word['api_attempts'] >= 1000:
             return True
     return False
 
 
-def normalize_words(words):
+def normalize_words(words) -> NormalizedWords:
     new_words = []
 
     for word in words:
@@ -162,6 +211,3 @@ def generate(table):
         get_answers_and_questions(words)
 
     return get_response(words)
-
-
-generate([[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 1, 1]])
