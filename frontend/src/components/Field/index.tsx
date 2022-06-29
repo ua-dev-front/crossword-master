abff30b93af7a6dbfa0684efe2b124d6f743c4c2
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-06-29 23:17:12 +0300

    Added onChange to Field

--- frontend/src/components/Field/index.tsx
+++ frontend/src/components/Field/index.tsx
@@ -3,8 +3,9 @@ import './styles.scss';
 type Props = {
   matrix: Array<Array<number | string>>;
   mode: 'draw' | 'erase' | 'answer';
+  onChange: (matrix: Array<Array<number | string>>) => void;
 };
 
-export default function Field({ matrix, mode }: Props) {
+export default function Field({ matrix, mode, onChange }: Props) {
   return <div className='field'>Field</div>;
 }
