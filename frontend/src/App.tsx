a21b0e936e94eaeb10e9b968e8f3dfc8721c0be2
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-07-06 21:04:47 +0300

    Add Button example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Button from './components/Button';
 
 function App() {
-  return <p>Under development</p>;
+  return (
+    <>
+      <p>Under development</p>
+      <p>Button example:</p>
+      <Button label='Click me' onClick={() => console.log('clicked')} />
+    </>
+  );
 }
 
 export default App;
