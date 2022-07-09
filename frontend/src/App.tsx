b3aea499e5acca53d2a9df69dc23f6b64e55bc94
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-07-09 22:09:45 +0300

    Cell implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
+import Cell from './components/Cell';
+import Corner from './types/corner';
+
 function App() {
-  return <p>Under development</p>;
+  return (
+    <>
+      <p>Under development</p>
+      <p>Cell example:</p>
+      <Cell
+        data={{
+          editable: true,
+          filled: false,
+          onEdited: (filled) => console.log(filled),
+        }}
+      />
+      <hr />
+      <Cell
+        data={{
+          editable: true,
+          filled: true,
+          onEdited: (filled) => console.log(filled),
+        }}
+      />
+      <hr />
+      <Cell
+        data={{
+          editable: false,
+          content: {
+            letter: 'A',
+            number: 1,
+          },
+        }}
+        corner={Corner.TopLeft}
+      />
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/Cell/index.tsx
+++ frontend/src/components/Cell/index.tsx
@@ -1,3 +1,5 @@
+import Corner from '../../types/corner';
+
 import './styles.scss';
 
 type Props = {
@@ -5,14 +7,38 @@ type Props = {
     | {
         editable: true;
         filled: boolean;
+        onEdited: (filled: boolean) => void;
       }
     | {
         editable: false;
         content: { letter: string | null; number: number | null } | null;
       };
-  onEdited: (filled: boolean) => void;
+  corner?: Corner;
 };
 
-export default function Cell({ data, onEdited }: Props) {
-  return <div className='cell'>Cell</div>;
+export default function Cell({ data, corner }: Props) {
+  const { editable } = data;
+  const content = !editable ? data.content : null;
+  const filled = (editable && data.filled) || (!editable && content);
+
+  const classes = [
+    'cell',
+    `cell--${filled ? 'filled' : 'empty'}`,
+    corner && `cell--${corner}`,
+  ];
+
+  return (
+    <div
+      className={classes.join(' ')}
+      onClick={() => editable && data.onEdited(!filled)}
+      tabIndex={0}
+    >
+      {!editable && content?.letter && (
+        <span className='cell__letter'>{content.letter}</span>
+      )}
+      {!editable && content?.number && (
+        <span className='cell__number'>{content.number}</span>
+      )}
+    </div>
+  );
 }
