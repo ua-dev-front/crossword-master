0105c5f2159c629fffb99d682d7c559e208b5d45
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-07-21 22:22:08 +0300

    Add Grid work examples

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,102 @@ import React, { useState } from 'react';
 import Button from './components/Button';
 import Cell from './components/Cell';
 import Corner from './types/corner';
+import Grid from './components/Grid';
+import Mode from './types/mode';
+
+const answerMatrix = [
+  [
+    { letter: 'D', number: 1 },
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    { letter: 'I', number: null },
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    { letter: 'S', number: null },
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    { letter: 'C', number: 2 },
+    { letter: 'R', number: null },
+    { letter: 'O', number: null },
+    { letter: 'S', number: 3 },
+    { letter: 'S', number: null },
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    null,
+    null,
+    { letter: 'O', number: null },
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    null,
+    null,
+    { letter: 'N', number: null },
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  ...[...Array(4)].map(() => [...Array(10)].map(() => null)),
+];
+
+const puzzleMatrix = answerMatrix.map((row) =>
+  row.map((cell) => (cell ? { number: cell.number } : null))
+);
+
+const emptyMatrix = [...Array(10)].map(() => [...Array(10)].map(() => false));
 
 function App() {
   const [filled, setFilled] = useState(false);
+  const [matrix, setMatrix] = useState(emptyMatrix);
+
+  const handleMatrixChange = (row: number, column: number) => {
+    const newMatrix = [...matrix];
+
+    newMatrix[row][column] = !newMatrix[row][column];
+
+    setMatrix(newMatrix);
+  };
 
   return (
     <>
@@ -14,7 +107,7 @@ function App() {
         data={{
           editable: true,
           filled,
-          onEdited: (filled: boolean) => setFilled(filled),
+          onEdited: () => setFilled(!filled),
         }}
       />
       <hr />
@@ -30,6 +123,21 @@ function App() {
       />
       <p>Button example:</p>
       <Button label='Click me' onClick={() => console.log('clicked')} />
+      <hr />
+      <p>Grid edit example:</p>
+      <Grid
+        mode={Mode.Draw}
+        matrix={matrix}
+        onChange={(row: number, column: number) => {
+          handleMatrixChange(row, column);
+        }}
+      />
+      <hr />
+      <p>Grid puzzle example:</p>
+      <Grid mode={Mode.Puzzle} matrix={puzzleMatrix} />
+      <hr />
+      <p>Grid answer example:</p>
+      <Grid mode={Mode.Answer} matrix={answerMatrix} />
     </>
   );
 }
