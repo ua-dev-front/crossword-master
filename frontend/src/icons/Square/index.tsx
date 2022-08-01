6f829ba6e2b4943445bb14596f36d77b98cc9a49
TeksPeks <verstyukm@gmail.com>
2022-08-01 20:15:42 +0300

    small fixes

--- frontend/src/icons/Square/index.tsx
+++ frontend/src/icons/Square/index.tsx
@@ -1,16 +1,5 @@
 import './styles.scss';
 
 export default function Square() {
-  return (
-    <svg
-      width='16'
-      height='16'
-      viewBox='0 0 16 16'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
-      className='icon-square'
-    >
-      <rect x='0.5' y='0.5' width='15' height='15' rx='1.5' />
-    </svg>
-  );
+  return <span className='icon-square' />;
 }
--- frontend/src/icons/SquareLetter/index.tsx
+++ frontend/src/icons/SquareLetter/index.tsx
@@ -8,6 +8,7 @@ export default function SquareLetter() {
       viewBox='0 0 16 16'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
+      className='icon-square-letter'
     >
       <g>
         <rect x='0.5' y='0.5' width='15' height='15' rx='1.5' />
