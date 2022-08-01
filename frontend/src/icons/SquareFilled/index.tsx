ddb887aa4831dbb351000ce7ec3eedc9d0662389
TeksPeks <verstyukm@gmail.com>
2022-08-01 20:21:59 +0300

    icons as spans

--- frontend/src/icons/SquareFilled/index.tsx
+++ frontend/src/icons/SquareFilled/index.tsx
@@ -1,16 +1,5 @@
 import './styles.scss';
 
 export default function SquareFilled() {
-  return (
-    <svg
-      width='16'
-      height='16'
-      viewBox='0 0 16 16'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
-      className='icon-square-filled'
-    >
-      <rect width='16' height='16' rx='2' />
-    </svg>
-  );
+  return <span className='icon-square-filled' />;
 }
--- frontend/src/icons/SquareLetter/index.tsx
+++ frontend/src/icons/SquareLetter/index.tsx
@@ -1,21 +1,5 @@
 import './styles.scss';
 
 export default function SquareLetter() {
-  return (
-    <svg
-      width='16'
-      height='16'
-      viewBox='0 0 16 16'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
-      className='icon-square-letter'
-    >
-      <g>
-        <rect x='0.5' y='0.5' width='15' height='15' rx='1.5' />
-        <text x='30%' y='80%'>
-          A
-        </text>
-      </g>
-    </svg>
-  );
+  return <span className='icon-square-letter'>A</span>;
 }
