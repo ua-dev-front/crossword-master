1be43fa1a70ef8a68c29c9075b4ad4daea89bc34
TeksPeks <verstyukm@gmail.com>
2022-08-08 22:32:14 +0300

    fix without external dependencies

--- frontend/src/components/TextField/index.tsx
+++ frontend/src/components/TextField/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactTextareaAutosize from 'react-textarea-autosize';
+import React, { useEffect, useRef } from 'react';
 import classnames from 'classnames';
 import './styles.scss';
 
@@ -14,15 +13,35 @@ export default function TextField({
   content,
   onChange,
 }: Props) {
+  const textareaRef = useRef(null);
+
   const classes = classnames('text-field', isEditable && 'text-field_editable');
   const placeholder = 'TBD';
 
+  const setHeight = (element: HTMLTextAreaElement) => {
+    // Set height to zero to get real height of content, instead of strictly set height
+    element.style.height = '0';
+
+    const elementStyles = window.getComputedStyle(element);
+    const fullHeight =
+      element.scrollHeight -
+      parseInt(elementStyles.paddingTop) -
+      parseInt(elementStyles.paddingBottom);
+
+    element.style.height = `${fullHeight}px`;
+  };
+
+  useEffect(() => {
+    if (isEditable && textareaRef.current) setHeight(textareaRef.current);
+  }, [content, isEditable]);
+
   return isEditable ? (
-    <ReactTextareaAutosize
+    <textarea
       className={classes}
       placeholder={placeholder}
       value={content}
       onChange={(e) => onChange?.(e.target.value)}
+      ref={textareaRef}
     />
   ) : (
     <div className={classes}>{content}</div>
