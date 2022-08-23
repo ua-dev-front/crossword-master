d5fdd5192e9889c2ce9c7c29c0199bd7af8b80c8
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-08-24 00:00:59 +0300

    `Loader` component

---
+++ frontend/src/components/Loader/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import classnames from 'classnames';
+import './style.scss';
+
+type Props = {
+  label: string;
+  isLoading: boolean;
+};
+
+export default function Loader({ label, isLoading }: Props) {
+  const className = 'loader';
+  const classes = classnames(className, !isLoading && `${className}_hidden`);
+
+  return (
+    <div className={classes}>
+      <div className={`${className}__label`}>{label}</div>
+    </div>
+  );
+}
