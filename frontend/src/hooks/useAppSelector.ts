03b4b2498454f2162e19a862f198d1d6c38b2fe0
TeksPeks <verstyukm@gmail.com>
2022-09-02 10:39:19 +0300

    small fix

--- frontend/src/hooks/useAppSelector.ts
+++ frontend/src/hooks/useAppSelector.ts
@@ -1,4 +1,4 @@
-import { TypedUseSelectorHook, useSelector } from 'react-redux'
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import type { RootState } from 'store';
 
 const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
