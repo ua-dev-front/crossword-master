5e601a06eb84253e7d8a89084632f3b20a80ea26
TeksPeks <verstyukm@gmail.com>
2022-09-15 22:02:19 +0300

    export reducer

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -119,4 +119,6 @@ const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+export const { updateQuestion } = generalSlice.actions;
+
 export default store;
