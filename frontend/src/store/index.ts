4e8528e7a237419c50babd3e3fa028767ff3bc11
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-09-16 20:31:44 +0300

    Export reducers

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -116,4 +116,5 @@ const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+export const { showConfirmation, dismissConfirmation } = generalSlice.actions;
 export default store;
