e9094a3b88116865e12c61048110debfc6e5200e
TeksPeks <verstyukm@gmail.com>
2022-09-18 19:18:18 +0300

    add cors handler for api

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from typing import TypedDict
 
 from app_types import Direction, GenerateResponse, Position, SolveResponse, SolveWord, Table
@@ -34,6 +35,8 @@ class SolveData(TypedDict):
 
 # Endpoints
 app = Flask(__name__)
+CORS(app, resources={
+     r'*': {'origins': ['http://localhost:3000', 'https://crossword-master.org']}})
 
 
 def request_handler(func: callable):
