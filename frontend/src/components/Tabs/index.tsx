42482992bf94815dfcde521ad7a402a53e47f31b
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-09-26 15:20:12 +0300

    Create `Tabs` component prototype

---
+++ frontend/src/components/Tabs/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Tab from 'components/Tab';
+import LeftArrow from 'icons/LeftArrow';
+import './styles.scss';
+
+type Props = {
+  selectedTab: {
+    label: string;
+    icon: React.ReactNode;
+  };
+  secondTab: {
+    label: string;
+    onClick: () => void;
+    icon: React.ReactNode;
+  };
+  onEditClick?: () => void;
+};
+
+export default function Tabs({ selectedTab, secondTab, onEditClick }: Props) {
+  return (
+    <div className='tabs'>
+      {onEditClick ? (
+        <Tab
+          label='Edit'
+          isSelected={false}
+          onClick={() => onEditClick()}
+          icon={<LeftArrow />}
+        />
+      ) : (
+        <div />
+      )}
+      <Tab
+        label={selectedTab.label}
+        isSelected={true}
+        icon={selectedTab.icon}
+      />
+      <Tab
+        label={secondTab.label}
+        isSelected={false}
+        onClick={secondTab.onClick}
+        icon={secondTab.icon}
+      />
+    </div>
+  );
+}
