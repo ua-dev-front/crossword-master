27b78d997bbd6c60e85ececb01d9e0641903759b
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-09-30 22:56:17 +0300

    Non-movable Tabs prototype

--- frontend/src/components/Tabs/index.tsx
+++ frontend/src/components/Tabs/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classnames from 'classnames';
 import Tab from 'components/Tab';
 import LeftArrow from 'icons/LeftArrow';
 import './styles.scss';
@@ -21,6 +22,22 @@ export default function Tabs({
   secondaryTab,
   onEditClick,
 }: Props) {
+  const addGerundToVerb = (verb: string) => {
+    if (verb.endsWith('ing')) {
+      return verb;
+    }
+
+    if (verb.endsWith('e')) {
+      return verb.slice(0, verb.length - 1) + 'ing';
+    }
+
+    if (verb.endsWith('ie')) {
+      return verb.slice(0, verb.length - 2) + 'ying';
+    }
+
+    return verb + 'ing';
+  };
+
   return (
     <div className='tabs'>
       {onEditClick ? (
@@ -35,13 +52,28 @@ export default function Tabs({
         <div />
       )}
       {[selectedTab, secondaryTab].map((tab, index) => (
-        <Tab
-          key={index}
-          label={tab.label}
-          isSelected={tab === selectedTab}
-          onClick={tab === secondaryTab ? secondaryTab.onClick : undefined}
-          icon={tab.icon}
-        />
+        <div key={index} className='tabs__item'>
+          <div className='tabs__tab'>
+            <Tab
+              label={tab.label}
+              isSelected={tab === selectedTab}
+              onClick={tab === secondaryTab ? secondaryTab.onClick : undefined}
+              icon={tab.icon}
+            />
+          </div>
+          {[selectedTab, secondaryTab].map((hiddenTab, indexOfHiddenTab) => (
+            <div
+              key={indexOfHiddenTab}
+              className={classnames('tabs__tab', 'tabs__tab_hidden')}
+            >
+              <Tab
+                label={addGerundToVerb(hiddenTab.label)}
+                isSelected={true}
+                icon={hiddenTab.icon}
+              />
+            </div>
+          ))}
+        </div>
       ))}
     </div>
   );
