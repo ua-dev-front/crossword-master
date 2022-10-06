44655ca2ecaeb93da92b27df395c2cdebe91d122
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-10-06 18:16:13 +0300

    Fix Tabs transition
    use DrawingOrErasingView for drawing and erasing mode, get rid of the set views that caused rendering and broke the animation

--- frontend/src/components/App/index.tsx
+++ frontend/src/components/App/index.tsx
@@ -8,37 +8,46 @@ import {
 } from 'store';
 import useAppDispatch from 'hooks/useAppDispatch';
 import useAppSelector from 'hooks/useAppSelector';
-import DrawingView from 'views/DrawingView';
-import ErasingView from 'views/ErasingView';
+import DrawingOrErasingView from 'views/DrawingOrErasingView';
 import Layout from 'components/Layout';
 
 function App() {
   const dispatch = useAppDispatch();
   const mode = useAppSelector((state) => state.general.mode);
   const grid = useAppSelector((state) => state.general.grid);
-  const viewByMode = {
-    [Mode.Draw]: (
-      <DrawingView
-        grid={grid}
-        onModeChange={() => dispatch(switchToErasing())}
-        onCellChange={(row, column) => dispatch(fillCell({ row, column }))}
-      />
-    ),
-    [Mode.Erase]: (
-      <ErasingView
-        grid={grid}
-        onModeChange={() => dispatch(switchToDrawing())}
-        onCellChange={(row, column) => dispatch(eraseCell({ row, column }))}
-      />
-    ),
-    [Mode.EnterQuestions]: <></>,
-    [Mode.Answer]: <></>,
-    [Mode.Puzzle]: <></>,
+
+  const getCurrentView = () => {
+    switch (mode) {
+      case Mode.Draw:
+      case Mode.Erase:
+        return (
+          <DrawingOrErasingView
+            mode={mode}
+            grid={grid}
+            onModeChange={() =>
+              dispatch(
+                (mode === Mode.Draw ? switchToErasing : switchToDrawing)(),
+              )
+            }
+            onCellChange={(row, column) =>
+              dispatch(
+                (mode === Mode.Draw ? fillCell : eraseCell)({ row, column }),
+              )
+            }
+          />
+        );
+      case Mode.EnterQuestions:
+        return <></>;
+      case Mode.Answer:
+        return <></>;
+      case Mode.Puzzle:
+        return <></>;
+    }
   };
 
-  const currentView = viewByMode[mode];
-
-  return <Layout title='Crossword Generator & Solver'>{currentView}</Layout>;
+  return (
+    <Layout title='Crossword Generator & Solver'>{getCurrentView()}</Layout>
+  );
 }
 
 export default App;
