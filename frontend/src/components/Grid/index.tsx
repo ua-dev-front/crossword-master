ae5c9d7a9767ddb7aa93a6f7a3e66fe336b8a753
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-10-09 11:47:00 +0300

    Move `mode ===` from ternary operator
    to avoid repetitions

--- frontend/src/components/Grid/index.tsx
+++ frontend/src/components/Grid/index.tsx
@@ -37,9 +37,7 @@ export default function Grid(props: Props) {
       case Mode.Draw:
       case Mode.Erase:
         return {
-          editable: matrix[row][column]
-            ? mode === Mode.Erase
-            : mode === Mode.Draw,
+          editable: mode === (matrix[row][column] ? Mode.Erase : Mode.Draw),
           filled: matrix[row][column],
         };
       case Mode.Puzzle:
