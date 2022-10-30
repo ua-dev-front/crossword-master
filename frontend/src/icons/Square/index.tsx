097906e8eb3f48d29de1a702dd93cf3e82ec2407
TeksPeks <verstyukm@gmail.com>
2022-10-30 13:40:50 +0300

    add transition to answer/puzzle tabs

--- frontend/src/icons/Square/index.tsx
+++ frontend/src/icons/Square/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
 import './styles.scss';
+import TransitionContainer from 'components/TransitionContainer';
 
 export type Props = {
   isFilled: boolean;
@@ -14,7 +15,15 @@ export default function Square({ isFilled, content }: Props) {
     isFilled && 'icon-square_filled',
   );
 
-  return <span className={classes}>{content}</span>;
+  return (
+    <span className={classes}>
+      {
+        <TransitionContainer
+          items={[{ key: 'content', content, hide: !content }]}
+        />
+      }
+    </span>
+  );
 }
 
 Square.defaultProps = {
