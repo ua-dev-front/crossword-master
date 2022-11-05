9d35c6930003e3fa516ad612a8bf57bd4c9eb6fd
TeksPeks <verstyukm@gmail.com>
2022-11-05 19:47:28 +0200

    fix `get_table_size`

--- backend/backtracking.py
+++ backend/backtracking.py
@@ -1,5 +1,5 @@
 from app_types import Direction, LoadOptions, Pattern, Position, WordLocation
-from helpers import shift_position
+from helpers import get_other_direction, shift_position
 
 __all__ = ['solve']
 
@@ -30,10 +30,11 @@ def get_word_pattern(table: BackTrackTable, direction: Direction, start_position
 
 def get_table(locations: list[WordLocation]) -> BackTrackTable:
     def get_last_position(location: WordLocation) -> Position:
-        return shift_position(location.first_letter, location.type, location.length)
+        after_first_letter = shift_position(location.first_letter, get_other_direction(location.type))
+        return shift_position(after_first_letter, location.type, location.length)
 
     def get_table_size() -> int:
-        return max((coord for location in locations for coord in get_last_position(location)), default=0) + 1
+        return max((coord for location in locations for coord in get_last_position(location)), default=0)
 
     return [[None for _ in range(get_table_size())] for _ in range(get_table_size())]
 
