80a8b29b4857eb56a9ebd04686e43834e9aeed8d
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-11-27 19:47:42 +0200

    Consider order from API

--- backend/api.py
+++ backend/api.py
@@ -13,8 +13,8 @@ PART_OF_SPEECH_ORDER = ['n', 'adj', 'v', 'adv', 'u']
 def sort_by_part_of_speech(response: list[dict]) -> list[dict]:
     return sorted(
         response,
-        key=lambda item: next((index for index, tag in enumerate(PART_OF_SPEECH_ORDER) if tag in item.get('tags', [])),
-                              len(PART_OF_SPEECH_ORDER))
+        key=lambda item: next((PART_OF_SPEECH_ORDER.index(tag) for tag in item.get('tags', [])
+                               if tag in PART_OF_SPEECH_ORDER), len(PART_OF_SPEECH_ORDER))
     )
 
 
